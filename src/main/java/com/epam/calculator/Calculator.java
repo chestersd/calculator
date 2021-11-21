f14582425d92d4f53d0d5c1a6dc29b3ea0c1bf0b
Eugene Kadyko <chester_sd@mail.ru>
2021-11-22 00:14:51 +0300

    added Subtract and other

--- src/main/java/com/epam/calculator/Calculator.java
+++ src/main/java/com/epam/calculator/Calculator.java
@@ -1,4 +1,15 @@
 package com.epam.calculator;
 
 public class Calculator {
+    public int add(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public int subtract(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public double multiply(double firstNumber, double secondNumber) {
+        return firstNumber * secondNumber;
+    }
 }
--- src/test/java/com/epam/calculator/CalculatorTest.java
+++ src/test/java/com/epam/calculator/CalculatorTest.java
@@ -1,4 +1,79 @@
 package com.epam.calculator;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CalculatorTest {
+
+    @Test
+    public void testAddShouldAddWhenNumbersPositive(){
+        //given
+        Calculator calculator = new Calculator();
+
+        //when
+        int summ = calculator.add(1, 2);
+
+        //then
+        Assert.assertEquals(3, summ);
+    }
+
+    @Test
+    public void testAddShouldAddWhenNumbersNegative(){
+        //given
+        Calculator calculator = new Calculator();
+
+        //when
+        int summ = calculator.add(-5, -3);
+
+        //then
+        Assert.assertEquals(-8, summ);
+    }
+
+    @Test
+    public void testSubtractShouldSubtractWhenNumbersPositive(){
+        //given
+        Calculator calculator = new Calculator();
+
+        //when
+        int difference = calculator.subtract(7,5);
+
+        //then
+        Assert.assertEquals(2, difference);
+    }
+
+    @Test
+    public void testSubtractShouldSubtractWhenNumbersNegative(){
+        //given
+        Calculator calculator = new Calculator();
+
+        //when
+        int difference = calculator.subtract(-7,-5);
+
+        //then
+        Assert.assertEquals(-2, difference);
+    }
+
+    @Test
+    public void testMultiplyShouldMultiplyWhenNumbersInteger(){
+        //given
+        Calculator calculator = new Calculator();
+
+        //when
+        double product = calculator.multiply(7,5);
+
+        //then
+        Assert.assertEquals(35, product);
+    }
+
+    @Test
+    public void testMultiplyShouldMultiplyWhenNumbersFraction(){
+        //given
+        Calculator calculator = new Calculator();
+
+        //when
+        double product = calculator.multiply(7.5, 3.9);
+
+        //then
+        Assert.assertEquals(35, product);
+    }
 }
